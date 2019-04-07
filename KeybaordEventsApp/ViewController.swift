72f76afdcffd7337c77749658fa4bc124ad4a97a
Kalaivani <vkalaiv@capelit53165287.local>
2019-04-07 14:44:24 +0530

    Initial Commit

---
+++ KeybaordEventsApp/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  KeybaordEventsApp
+//
+//  Created by Kalaivani, Velusamy (623-Extern) on 07/04/19.
+//  Copyright © 2019 Kalaivani, Velusamy (623-Extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func outsideClicked(_ sender: Any) {
+        textField.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+
+}
+
